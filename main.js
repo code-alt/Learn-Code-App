df741fef2a338d1a25be3acd1456508a3a51bdf1
code-alt <altthatguy@gmail.com>
2021-05-20 15:47:20 +0000

    new plan: just get this up and running

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get("/:route:", (req, res) => {
-    if (require('fs').existsSync(__dirname + "/static" + req.path)) {
-        res.sendFile(__dirname + "/static" + req.path);
-    } else {
-        res.sendFile(__dirname + "/static/404/index.html")
-    }
-})
+app.use(express.static(__dirname + "/static"));
+
+app.get("*", (req,res) => {
+    res.sendFile(__dirname + "/static/404/index.html");
+});
 
 app.listen(3000 || process.env.PORT);
